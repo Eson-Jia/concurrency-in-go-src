bd239f6108c755784e7ce39e8ba8994a70e9e11e
jiayuxin <13520723393@163.com>
2020-04-01 17:04:49 +0800

    找出导致不能一直活锁的原因

--- an-introduction-to-concurrency/why-is-concurrency-hard/deadlocks-livelocks-and-starvation/livelock/fig-livelock-hallway.go
+++ an-introduction-to-concurrency/why-is-concurrency-hard/deadlocks-livelocks-and-starvation/livelock/fig-livelock-hallway.go
@@ -11,7 +11,7 @@ import (
 func main() {
 	cadence := sync.NewCond(&sync.Mutex{})
 	go func() {
-		for range time.Tick(time.Second * 2) {
+		for range time.Tick(time.Millisecond * 1) {
 			cadence.L.Lock()
 			cadence.Broadcast()
 			cadence.L.Unlock()
@@ -27,12 +27,18 @@ func main() {
 		atomic.AddInt32(dir, 1)
 		fmt.Fprintf(buffer, "%s,", dirName)
 		takeStep()
-		if atomic.LoadInt32(dir) == 1 {
+		if atomic.LoadInt32(dir) == 1 { // <1.1>
 			fmt.Fprint(buffer, "success")
 			return true
 		}
-		atomic.AddInt32(dir, -1)
-		takeStep()
+		takeStep()               // <1.2>
+		atomic.AddInt32(dir, -1) // <1.3>
+		// 如果 1.2 和 1.3 位置互换的话，死锁不容易出现
+		// 这是因为在两个 goroutine 在 takeStep 之后同步开始往下执行
+		// 但是如果一个 goroutine 很快执行完 dir -1之后,
+		//另一个 goroutine 才执行到 if 语句,那么条件为 true 就会 return true
+		// 进而退出活锁状态
+		// 不过这是不正确的,因为为了制造活锁,明确要求了两个问必须同时移动
 		return false
 	}
 	var right, left int32
@@ -45,16 +51,19 @@ func main() {
 	finished := sync.WaitGroup{}
 	walk := func(name string) {
 		defer finished.Done()
-		out := &bytes.Buffer{}
-		defer fmt.Println("test:", out)
+		out := bytes.NewBufferString(fmt.Sprintf("%v is trying to scoot:", name))
+		defer fmt.Println(out)
 		for i := 0; i < 5; i++ {
 			if tryLeft(out) || tryRight(out) {
 				return
 			}
 		}
+		fmt.Fprintf(out, "\n%v tosses her hands up in exasperation!", name)
 	}
+	start := time.Now()
 	finished.Add(2)
 	go walk("alice")
 	go walk("mike")
 	finished.Wait()
+	fmt.Println("duration:", time.Since(start))
 }
