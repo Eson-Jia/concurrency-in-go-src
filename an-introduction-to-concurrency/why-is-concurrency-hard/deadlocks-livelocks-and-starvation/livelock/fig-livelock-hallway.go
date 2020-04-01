544ff2cadf1d4c55edefde10038e5f4470511d9e
jiayuxin <13520723393@163.com>
2020-04-01 15:45:18 +0800

    自己实现活锁

--- an-introduction-to-concurrency/why-is-concurrency-hard/deadlocks-livelocks-and-starvation/livelock/fig-livelock-hallway.go
+++ an-introduction-to-concurrency/why-is-concurrency-hard/deadlocks-livelocks-and-starvation/livelock/fig-livelock-hallway.go
@@ -11,49 +11,50 @@ import (
 func main() {
 	cadence := sync.NewCond(&sync.Mutex{})
 	go func() {
-		for range time.Tick(1 * time.Millisecond) {
+		for range time.Tick(time.Second * 2) {
+			cadence.L.Lock()
 			cadence.Broadcast()
+			cadence.L.Unlock()
 		}
 	}()
-
 	takeStep := func() {
 		cadence.L.Lock()
 		cadence.Wait()
 		cadence.L.Unlock()
 	}
 
-	tryDir := func(dirName string, dir *int32, out *bytes.Buffer) bool { // <1>
-		fmt.Fprintf(out, " %v", dirName)
-		atomic.AddInt32(dir, 1) // <2>
-		takeStep()              // <3>
+	tryDir := func(buffer *bytes.Buffer, dir *int32, dirName string) bool {
+		atomic.AddInt32(dir, 1)
+		fmt.Fprintf(buffer, "%s,", dirName)
+		takeStep()
 		if atomic.LoadInt32(dir) == 1 {
-			fmt.Fprint(out, ". Success!")
+			fmt.Fprint(buffer, "success")
 			return true
 		}
+		atomic.AddInt32(dir, -1)
 		takeStep()
-		atomic.AddInt32(dir, -1) // <4>
 		return false
 	}
-
-	var left, right int32
-	tryLeft := func(out *bytes.Buffer) bool { return tryDir("left", &left, out) }
-	tryRight := func(out *bytes.Buffer) bool { return tryDir("right", &right, out) }
-	walk := func(walking *sync.WaitGroup, name string) {
-		var out bytes.Buffer
-		defer func() { fmt.Println(out.String()) }()
-		defer walking.Done()
-		fmt.Fprintf(&out, "%v is trying to scoot:", name)
-		for i := 0; i < 5; i++ { // <1>
-			if tryLeft(&out) || tryRight(&out) { // <2>
+	var right, left int32
+	tryLeft := func(buffer *bytes.Buffer) bool {
+		return tryDir(buffer, &left, "left")
+	}
+	tryRight := func(buffer *bytes.Buffer) bool {
+		return tryDir(buffer, &right, "right")
+	}
+	finished := sync.WaitGroup{}
+	walk := func(name string) {
+		defer finished.Done()
+		out := &bytes.Buffer{}
+		defer fmt.Println("test:", out)
+		for i := 0; i < 5; i++ {
+			if tryLeft(out) || tryRight(out) {
 				return
 			}
 		}
-		fmt.Fprintf(&out, "\n%v tosses her hands up in exasperation!", name)
 	}
-
-	var peopleInHallway sync.WaitGroup // <3>
-	peopleInHallway.Add(2)
-	go walk(&peopleInHallway, "Alice")
-	go walk(&peopleInHallway, "Barbara")
-	peopleInHallway.Wait()
+	finished.Add(2)
+	go walk("alice")
+	go walk("mike")
+	finished.Wait()
 }
